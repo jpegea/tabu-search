3c8073a269786e66ea890a6f54535d6def320a79
carlesatm <carlesarnautm@gmail.com>
2024-12-20 17:04:04 +0100

    tabu demo

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from structure import instance, solution
-from algorithms import grasp
+from algorithms import tabu
 import random
+import time
 
 def executeInstance():
-    path = "instances/MDG-a_2_n500_m50.txt"
+    m=10
+    path = "instances/MDG-a_4_100_m10.txt"
     inst = instance.readInstance(path)
-    sol = grasp.execute(inst, 10, -1)
+    sol = tabu.execute(inst, 100, 1, m)
     print("\nBEST SOLUTION:")
     solution.printSolution(sol)
 
+inicio = time.time()
 if __name__ == '__main__':
     random.seed(1)
     executeInstance()
-
+final = time.time()
+print(final-inicio)
