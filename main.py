1964d9fbbcfaf92adea344dcb070e137c10718c5
jpegea <joaquimpascualegea@gmail.com>
2024-12-22 01:01:26 +0100

    llevar guarraes del main

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 from structure import instance, solution
-from algorithms import tabu
+from algorithms import tabusearch, grasp
 import random
-import time
 
 def executeInstance():
-    m=10
     path = "instances/MDG-a_2_n500_m50.txt"
     inst = instance.readInstance(path)
-    sol = tabu.execute(inst, 1000, 0, m)
+    sol = tabusearch.execute(inst, 1000, 10)
+    # sol = grasp.execute(inst, 1000, 0.2)
     print("\nBEST SOLUTION:")
     solution.printSolution(sol)
 
-inicio = time.time()
 if __name__ == '__main__':
     random.seed(1)
     executeInstance()
-final = time.time()
-print(final-inicio)
