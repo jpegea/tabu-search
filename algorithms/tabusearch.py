4dee0831959e392e6ff19f935115b233ff9e85be
jpegea <joaquimpascualegea@gmail.com>
2024-12-22 11:51:23 +0100

    added alert if we return to the best found solution

--- algorithms/tabusearch.py
+++ algorithms/tabusearch.py
@@ -7,6 +7,8 @@ def execute(inst, iters, tabutenure):
     sol = ctabusearch.greedySolution(inst)
     for i in range(iters):
         tabulsbestimp.move(sol, tabulist)
+        if best is not None and sol['sol'] == best['sol']:
+            print("Hem tornat a al pico")
         if best is None or best['of'] < sol['of']:
             if best is not None:
                 print(str(round(best['of'],2))+'\t-> '+str(round(sol['of'],2)))
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from structure import instance, solution
 from algorithms import tabusearch, grasp
-import random
+import random, time
 
 def executeInstance():
     path = "instances/MDG-a_2_n500_m50.txt"
     inst = instance.readInstance(path)
-    sol = tabusearch.execute(inst, 1000, 10)
-    # sol = grasp.execute(inst, 1000, 0.2)
+    sol = tabusearch.execute(inst, 10000, 20)
+    #sol = grasp.execute(inst, 1000, 0.2)
     print("\nBEST SOLUTION:")
     solution.printSolution(sol)
 
 if __name__ == '__main__':
-    random.seed(1)
+    # random.seed(1)
+    start = time.time()
     executeInstance()
+    total_time = time.time() - start
+    print("Time:", total_time)
