e2854d6d0f02fbb7c0966c6f2b7cdb93ce16fe30
jpegea <joaquimpascualegea@gmail.com>
2024-12-24 15:20:33 +0100

    do not print any output

--- algorithms/grasp.py
+++ algorithms/grasp.py
@@ -4,11 +4,11 @@ from localsearch import lsbestimp
 def execute(inst, iters, alpha):
     best = None
     for i in range(iters):
-        print("Iter "+str(i+1)+": ", end="")
+        # print("Iter "+str(i+1)+": ", end="")
         sol = cgrasp.construct(inst, alpha)
-        print("C -> "+str(round(sol['of'], 2)), end=", ")
+        # print("C -> "+str(round(sol['of'], 2)), end=", ")
         lsbestimp.improve(sol)
-        print("LS -> "+str(round(sol['of'], 2)))
+        # print("LS -> "+str(round(sol['of'], 2)))
         if best is None or best['of'] < sol['of']:
             best = sol
     return best
--- algorithms/tabusearch.py
+++ algorithms/tabusearch.py
@@ -7,24 +7,18 @@ def execute(inst, iters, tabutenure, initial="greedy", alpha=0):
     sol = chooseInitialSolution(inst, initial, alpha)
     best = {'sol':sol['sol'].copy(), 'of':sol['of']}
     tabulistAtBest = None
-
     for i in range(iters):
-        
         tsfirstimp.move(sol, tabulist)
-
         # Alerta de ciclado
         if sol['sol'] == best['sol']:
             if tabulistAtBest is not None and set(tabulist) == set(tabulistAtBest):
                 print("\033[41mCICLADO!\033[0m en Iter:", i)
-            else:
-                # print("\033[105mPosible ciclado\033[0m en Iter:", i)
-                pass
-
+                break
+            # print("\033[105mPosible ciclado\033[0m en Iter:", i)
         if best['of'] < sol['of']:
-            print(str(round(best['of'],2))+'\t-> '+str(round(sol['of'],2)))
-            best = {'sol':sol['sol'].copy(), 'of':sol['of']}
+            # print(str(round(best['of'],2))+'\t-> '+str(round(sol['of'],2)))
+            best = {'sol' : sol['sol'].copy(), 'of' : sol['of']}
             tabulistAtBest = tabulist.copy()
-        
     return best
 
 def chooseInitialSolution(inst, initial, alpha):
@@ -38,5 +32,4 @@ def chooseInitialSolution(inst, initial, alpha):
         sol = ctabusearch.graspSolution(inst, alpha)
     else:
         sol = ctabusearch.greedySolution(inst)
-
     return sol
