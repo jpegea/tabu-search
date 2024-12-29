a95acdca7865ab247b4ee87519bb82676aee6a29
jpegea <joaquimpascualegea@gmail.com>
2024-12-29 12:54:08 +0100

    tenure = 30,40 i alpha = 0.4,0.7

--- test100.py
+++ test100.py
@@ -4,10 +4,10 @@ import random, time
 
 def printOutput(alg, sol, iters):
     print(alg, end = ', ')
-    print(str(round(sol['of'], 2)), end=', ')
-    print(str(iters))
+    print("OF: " + str(round(sol['of'], 2)), end=', ')
+    print("Iters: " + str(iters))
 
-
+    
 def test(inst, testTime):
     alpha = [0.4, 0.7]
     tenure = [30, 40]
@@ -19,56 +19,36 @@ def test(inst, testTime):
         alg = "TS tenure = " + str(t)
         sol, iters = tabusearch.executeduring(inst, testTime, t)
         printOutput(alg, sol, iters)
-
         
+
 random.seed(1)
 testTime = 10
 
 start = time.time()
 
-path = "instances/MDG-a_2_n500_m50.txt"
+path = "instances/MDG-a_1_100_m10.txt"
 inst = instance.readInstance(path)
-print("\nInstance 2,", path)
+print("\nInstance 1,", path)
 test(inst, testTime)
 
-path = "instances/MDG-a_5_n500_m50.txt"
+path = "instances/MDG-a_4_100_m10.txt"
 inst = instance.readInstance(path)
-print("\nInstance 5,", path)
+print("\nInstance 4,", path)
 test(inst, testTime)
 
-path = "instances/MDG-a_6_n500_m50.txt"
+path = "instances/MDG-a_10_100_m10.txt"
 inst = instance.readInstance(path)
-print("\nInstance 6,", path)
+print("\nInstance 10,", path)
 test(inst, testTime)
 
-path = "instances/MDG-a_9_n500_m50.txt"
+path = "instances/MDG-a_12_100_m10.txt"
 inst = instance.readInstance(path)
-print("\nInstance 9,", path)
+print("\nInstance 12,", path)
 test(inst, testTime)
 
-path = "instances/MDG-a_13_n500_m50.txt"
+path = "instances/MDG-a_14_100_m10.txt"
 inst = instance.readInstance(path)
-print("\nInstance 13,", path)
-test(inst, testTime)
-
-path = "instances/MDG-a_16_n500_m50.txt"
-inst = instance.readInstance(path)
-print("\nInstance 16,", path)
-test(inst, testTime)
-
-path = "instances/MDG-a_17_n500_m50.txt"
-inst = instance.readInstance(path)
-print("\nInstance 17,", path)
-test(inst, testTime)
-
-path = "instances/MDG-a_19_n500_m50.txt"
-inst = instance.readInstance(path)
-print("\nInstance 19,", path)
-test(inst, testTime)
-
-path = "instances/MDG-a_20_n500_m50.txt"
-inst = instance.readInstance(path)
-print("\nInstance 20,", path)
+print("\nInstance 14,", path)
 test(inst, testTime)
 
 end = time.time() - start
